0f3b99be6fbedfaf19a6b1030475dee922d935ec
philingood <98781376+philingood@users.noreply.github.com>
2025-03-26 19:12:15 +0300

    rm useless lines

--- homelab/configuration.nix
+++ homelab/configuration.nix
@@ -79,16 +79,6 @@
     name = "iqn.2016-04.com.open-iscsi:${meta.hostname}";
   };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.rekcah = {
     isNormalUser = true;
@@ -137,8 +127,6 @@
 	user.email = "98781376+philingood@users.noreply.github.com";
     };
   };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
