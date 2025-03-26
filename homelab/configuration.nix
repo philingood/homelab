0c8ae0a6dbb3291ba22db33383d9e3216d3a03c2
philingood <98781376+philingood@users.noreply.github.com>
2025-03-26 20:21:01 +0300

    added nixvim and restructured

--- homelab/configuration.nix
+++ homelab/configuration.nix
@@ -8,29 +8,36 @@
   imports =
     [ # Include the results of the hardware scan.
     ];
-
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
   };
+  boot.loader = {
+    # Use the systemd-boot EFI boot loader.
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+  networking = {
+    hostName = meta.hostname; # Define your hostname.
+    # Pick only one of the below networking options.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  networking.hostName = meta.hostname; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ 80 ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    firewall.enable = false;
+  };
 
   # Set your time zone.
   time.timeZone = "Africa/Addis_Ababa";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -39,7 +46,6 @@
     keyMap = "us";
     #useXkbConfig = true; # use xkb.options in tty.
   };
-
   security = {
     sudo.configFile = ''
     rekcah ALL=(ALL) NOPASSWD: ALL
@@ -57,21 +63,21 @@
     role = "server";
     tokenFile = /var/lib/rancher/k3s/server/token;
     extraFlags = toString ([
-	    "--write-kubeconfig-mode \"0644\""
-	    "--cluster-init"
-	    "--disable servicelb"
-	    "--disable traefik"
-	    "--disable local-storage"
+      "--write-kubeconfig-mode \"0644\""
+      "--cluster-init"
+      "--disable servicelb"
+      "--disable traefik"
+      "--disable local-storage"
     ] ++ (if meta.hostname == "master0" then [] else [
-	      "--server https://master0:6443"
-    ]));
+        "--server https://master0:6443"
+      ]));
     clusterInit = (meta.hostname == "master0");
   };
-
   services.openiscsi = {
     enable = true;
     name = "iqn.2016-04.com.open-iscsi:${meta.hostname}";
   };
+  services.openssh.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.rekcah = {
@@ -106,53 +112,165 @@
     sing-box
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   programs.git = {
     enable = true;
     config = {
-    	user.name = "philingood";
-	user.email = "98781376+philingood@users.noreply.github.com";
+      user.name = "philingood";
+      user.email = "98781376+philingood@users.noreply.github.com";
+    };
+  };
+  programs.nixvim = {
+    enable = true;
+    colorschemes.catppuccin.enable = true;
+    plugins = {
+      lualine.enable = true;
+      bufferline.enable = true;
+      oil.enable = true;
+      neoscroll.enable = true;
+      treesitter.enable = true;
+      web-devicons.enable = true;
+      nvim-autopairs.enable = true;
+      lazygit.enable = true;
+      commentary.enable = true;
+      gitsigns = {
+        enable = true;
+        settings.current_line_blame = true;
+      };
+      which-key = {
+        enable = false;
+      };
+      telescope = {
+        enable = true;
+        extensions = {
+          fzf-native = {
+            enable = true;
+          };
+        };
+      };
+      nix.enable = true;
+      none-ls = {
+        enable = true;
+        settings = {
+          cmd = ["bash -c nvim"];
+          debug = true;
+        };
+        sources = {
+          code_actions = {
+            statix.enable = true;
+            gitsigns.enable = true;
+          };
+          diagnostics = {
+            statix.enable = true;
+            deadnix.enable = true;
+            checkstyle.enable = true;
+          };
+          formatting = {
+            alejandra.enable = true;
+            stylua.enable = true;
+            shfmt.enable = true;
+            nixpkgs_fmt.enable = true;
+          };
+          completion = {
+            luasnip.enable = true;
+            spell.enable = true;
+          };
+        };
+      };
+      lint = {
+        enable = true;
+        lintersByFt = {
+          text = ["vale"];
+          json = ["jsonlint"];
+          markdown = ["vale"];
+          dockerfile = ["hadolint"];
+        };
+      };
+      lsp = {
+        enable = true;
+        servers = {
+          marksman.enable = true; # Markdown
+          nil_ls.enable = true; # Nix
+          dockerls.enable = true; # Docker
+          bashls.enable = true; # Bash
+          yamlls.enable = true; # YAML
+          lua_ls = { # Lua
+            enable = true;
+            settings.telemetry.enable = false;
+          };
+        };
+      };
+      cmp = {
+        enable = true;
+        settings = {
+          completion = {
+            completeopt = "menu,menuone,noinsert";
+          };
+          autoEnableSources = true;
+          experimental = { ghost_text = true; };
+          performance = {
+            debounce = 60;
+            fetchingTimeout = 200;
+            maxViewEntries = 30;
+          };
+          snippet = { 
+            expand = ''
+            function(args)
+              require('luasnip').lsp_expand(args.body)
+            end
+            '';
+          };
+          formatting = { fields = [ "kind" "abbr" "menu" ]; };
+          sources = [
+            { name = "nvim_lsp"; }
+            {
+              name = "buffer"; # text within current buffer
+              option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+              keywordLength = 3;
+            }
+            # { name = "copilot"; } # enable/disable copilot
+            {
+              name = "path"; # file system paths
+              keywordLength = 3;
+            }
+          ];
+          window = {
+            completion = { border = "solid"; };
+            documentation = { border = "solid"; };
+          };
+          mapping = {
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            "<C-j>" = "cmp.mapping.select_next_item()";
+            "<C-k>" = "cmp.mapping.select_prev_item()";
+            "<C-e>" = "cmp.mapping.abort()";
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+            "<C-l>" = ''
+            cmp.mapping(function()
+              if luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+              end
+            end, { 'i', 's' })
+            '';
+            "<C-h>" = ''
+            cmp.mapping(function()
+              if luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+              end
+            end, { 'i', 's' })
+            '';
+          };
+        };
+      };
+      cmp-nvim-lsp.enable = true; # LSP
+      cmp-buffer.enable = true;
+      cmp-path.enable = true; # file system paths
+      cmp_luasnip.enable = true; # snippets
+      cmp-cmdline.enable = true; # autocomplete for cmdline
     };
   };
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ 80 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
